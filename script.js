28c58a2dcb4f1dc7d2ff1e9092692d3868fb4fc5
Quicka314 <93557196+Quicka314@users.noreply.github.com>
2021-11-22 19:40:57 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    this.fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+     .then(response => response.json())
+     .then(astronauts => {
+         let container = document.getElementById("container");
+         let str = ""
+        for (let i = 0; i < astronauts.length; i++){
+            str +=
+            `<div class="astronaut">
+            <div class="bio">
+                <h3>${astronauts[i].firstName} ${astronauts[i].lastName}</h3>
+                <ul>
+                    <li>Hours in space${astronauts[i].hoursInSpace}</li>
+                    <li>Active${astronauts[i].active}</li>
+                    <li>Skills${astronauts[i].skills.join(", ")}</li>
+                </ul>
+            </div>
+            <img class="avatar" src="${astronauts[i].picture}">
+            </div>`
+        }
+        container.innerHTML = str
+     })
+});
